46aece637f8283dcc053f14e1abb72f37db23919
rakeshsfdc-AI <grakeshsfdc@gmail.com>
2025-03-05 17:44:55 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+function function1(){
+if (opts.theme && full) {
+				s = '<div class="blockUI ' + opts.blockMsgClass + ' blockPage ui-dialog ui-widget ui-corner-all" style="z-index:'+(z+10)+';display:none;position:fixed">';
+				if ( opts.title ) {
+					s += '<div class="ui-widget-header ui-dialog-titlebar ui-corner-all blockTitle">'+(opts.title || '&nbsp;')+'</div>';
+				}
+				s += '<div class="ui-widget-content ui-dialog-content"></div>';
+				s += '</div>';
+			}
+}
