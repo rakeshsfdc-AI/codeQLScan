40b32c674e64b1712a05d1f5ef847e83738fbbe9
rakeshsfdc-AI <grakeshsfdc@gmail.com>
2025-05-12 01:13:46 +0530

    Create test121.js

---
+++ test121.js
@@ -0,0 +1,4 @@
+function(){
+if(b){var c=this._get(a,"altFormat")||
+this._get(a,"dateFormat"),e=this._getDate(a),f=this.formatDate(c,e,this._getFormatConfig(a));d(b).each(function(){d(this).val(f)})}
+}
