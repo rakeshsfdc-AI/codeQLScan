4c221c10e6d9fd3800c3bfc250dc54491f3eaf58
rakeshsfdc-AI <grakeshsfdc@gmail.com>
2025-05-12 18:43:55 +0530

    Create test121.js

---
+++ test121/test121.js
@@ -0,0 +1,9 @@
+(function( $, undefined ) {
+var atOffset, targetWidth, targetHeight, targetOffset, basePosition,
+		target = $( options.of ),
+		within = $.position.getWithinInfo( options.within ),
+		scrollInfo = $.position.getScrollInfo( within ),
+		targetElem = target[0],
+		collision = ( options.collision || "flip" ).split( " " ),
+		offsets = {};
+})
