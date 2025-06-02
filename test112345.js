73166af01d20f4258c44071e43e2f5034c13e7ab
rakeshsfdc-AI <grakeshsfdc@gmail.com>
2025-06-02 17:04:51 +0530

    Create test112345.js

---
+++ test112345.js
@@ -0,0 +1,17 @@
+ function(e) {
+var et = /<(?!area|br|col|embed|hr|img|input|link|meta|param)(([\w:]+)[^>]*)\/>/gi;
+            return x.access(this, function(e) {
+                var t = this[0] || {},
+                    n = 0,
+                    r = this.length;
+                if (e === undefined && 1 === t.nodeType) return t.innerHTML;
+                if ("string" == typeof e && !rt.test(e) && !lt[(tt.exec(e) || ["", ""])[1].toLowerCase()]) {
+                    e = e.replace(et, "<$1></$2>");
+                    try {
+                        for (; r > n; n++) t = this[n] || {}, 1 === t.nodeType && (x.cleanData(gt(t, !1)), t.innerHTML = e);
+                        t = 0
+                    } catch (i) {}
+                }
+                t && this.empty().append(e)
+            }, null, e, arguments.length)
+        }
