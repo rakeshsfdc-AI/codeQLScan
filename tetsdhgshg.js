9e5fda12fdcc7449036f9220221e24f07d13155e
rakeshsfdc-AI <grakeshsfdc@gmail.com>
2025-06-27 11:16:51 +0530

    Create tetsdhgshg.js

---
+++ tetsdhgshg.js
@@ -0,0 +1,10 @@
+function() {
+        var e, t, n, r, i, o, s = arguments[0] || {},
+            a = 1,
+            u = arguments.length,
+            l = !1;
+        for ("boolean" == typeof s && (l = s, s = arguments[1] || {}, a = 2), "object" == typeof s || x.isFunction(s) || (s = {}), u === a && (s = this, --a); u > a; a++)
+            if (null != (e = arguments[a]))
+                for (t in e) n = s[t], r = e[t], s !== r && (l && r && (x.isPlainObject(r) || (i = x.isArray(r))) ? (i ? (i = !1, o = n && x.isArray(n) ? n : []) : o = n && x.isPlainObject(n) ? n : {}, s[t] = x.extend(l, o, r)) : r !== undefined && (s[t] = r));
+        return s
+    }
