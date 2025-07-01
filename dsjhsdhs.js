d4df2a31d32c09209e5f264e96d6652037d986cc
rakeshsfdc-AI <grakeshsfdc@gmail.com>
2025-07-01 18:39:49 +0530

    Create dsjhsdhs.js

---
+++ dsjhsdhs.js
@@ -0,0 +1,11 @@
+function( inst ) {
+		var altFormat, date, dateStr,
+			altField = this._get( inst, "altField" );
+
+		if ( altField ) { // update alternate field too
+			altFormat = this._get( inst, "altFormat" ) || this._get( inst, "dateFormat" );
+			date = this._getDate( inst );
+			dateStr = this.formatDate( altFormat, date, this._getFormatConfig( inst ) );
+			$( altField ).val( dateStr );
+		}
+	}
